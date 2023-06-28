2994268e487782db96f47e7013e68596e9aa0d7b
Matias Rodriguez Lima <matias.rodriguez.lima@gmail.com>
2023-06-28 15:50:58 -0300

    probando

---
+++ index.ts
@@ -0,0 +1,21 @@
+export class Unidad {
+  version: number;
+  id: string;
+  nombre: string;
+  activo: boolean;
+  ultimaEdicion: number;
+
+  constructor(
+    version: number,
+    id: string,
+    nombre: string,
+    activo: boolean,
+    ultimaEdicion: number
+  ) {
+    this.version = version;
+    this.id = id;
+    this.nombre = nombre;
+    this.activo = activo;
+    this.ultimaEdicion = ultimaEdicion;
+  }
+}
