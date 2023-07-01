ec5b5d1d55ee657bd5d07ad66823cd0d7fd7cc77
Matias Rodriguez Lima <matias.rodriguez.lima@gmail.com>
2023-07-01 18:06:55 -0300

    v 36

--- functions/src/firebase/index.ts
+++
@@ -1,9 +0,0 @@
-import functions = require("firebase-functions");
-// const { google } = require("googleapis");
-import admin = require("firebase-admin");
-
-admin.initializeApp();
-
-// module.exports = { functions, google, admin };
-
-export {admin, functions};
