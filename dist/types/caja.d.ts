176b3aeaf23b83012152ec29e3860796198cf1ca
Matias Rodriguez Lima <matias.rodriguez.lima@gmail.com>
2023-09-19 14:59:51 -0300

    v 2.0.146

--- dist/types/caja.d.ts
+++ dist/types/caja.d.ts
@@ -1,5 +1,9 @@
 interface CajaDeposito {
     id: string;
+    fecha: number;
+    fechaString: string;
+    idUsuarioDepositante: string;
+    nombreUsuarioDepositante: string;
     cajaOrigen: string;
     cajaDestino: string;
     monto: number;
