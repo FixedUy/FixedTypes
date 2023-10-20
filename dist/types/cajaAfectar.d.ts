208fc064c8269793cda0e10c1f82b114ca067162
Matias Rodriguez Lima <matias.rodriguez.lima@gmail.com>
2023-10-19 20:02:41 -0400

    v 203

---
+++ dist/types/cajaAfectar.d.ts
@@ -0,0 +1,10 @@
+import { CajaTipoMovimiento } from "./cajaMovimientos";
+export interface CajaAfectar {
+    caja: string;
+    detalle: string;
+    moneda: string;
+    medioCobro: string;
+    monto: number;
+    tipo: CajaTipoMovimiento;
+    idDocumento: string;
+}
---
+++ src/types/cajaAfectar.ts
@@ -0,0 +1,11 @@
+import {CajaTipoMovimiento} from "./cajaMovimientos";
+
+export interface CajaAfectar {
+  caja: string;
+  detalle: string;
+  moneda: string;
+  medioCobro: string;
+  monto: number;
+  tipo: CajaTipoMovimiento;
+  idDocumento: string;
+}
